a0cb8271d7d754bfed3484be257f5700a4266edd
Yuan Cheung <zhangyuan@users.noreply.github.com>
2014-08-07 23:53:52 +0800

    receive push message

---
+++ app/src/main/java/com/evcheung/libs/notify/app/NotifyApp.java
@@ -0,0 +1,30 @@
+package com.evcheung.libs.notify.app;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVAnalytics;
+import com.avos.avoscloud.AVOSCloud;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class NotifyApp extends Application{
+    @Override
+    public void onCreate() {
+        String appId = null;
+        String appKey = null;
+
+        try {
+            InputStream inputStream = getAssets().open("app.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            appId = properties.getProperty("app_id");
+            appKey = properties.getProperty("app_key");
+        } catch (IOException e) {
+        }
+
+        AVOSCloud.initialize(this, appId, appKey);
+        AVAnalytics.enableCrashReport(this.getApplicationContext(), true);
+    }
+}
